00e87dbd1a6d868340c70429bb63e157d458f815
Adeola07 <88549439+Adeola07@users.noreply.github.com>
2022-03-30 10:16:44 +0100

    Update styling

--- src/Weather.js
+++ src/Weather.js
@@ -40,13 +40,26 @@ export default function Weather(props) {
     return (
       <div className="Weather">
         <form onSubmit={handleSubmit}>
-          <input
-            type="search"
-            placeholder=" Enter city here"
-            onChange={updateCity}
-          />
-          <input type="submit" value="Search" />
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder=" Enter city here"
+                onChange={updateCity}
+                autoFocus="on"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
         </form>
+
         <WeatherDetails data={weatherData} />
       </div>
     );
