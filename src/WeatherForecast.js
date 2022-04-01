1e2ad2d2c350f57862cefb10becf2e23b2569597
Adeola07 <88549439+Adeola07@users.noreply.github.com>
2022-04-01 09:13:12 +0100

    Create WeatherForecast.js

---
+++ src/WeatherForecast.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0].dt} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "ce11c2b5d5b0cf5bfb3d822d9d4e493c";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
+}
