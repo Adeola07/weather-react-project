51ecddf79d859e7d596287f389fa523ae7796733
Adeola07 <88549439+Adeola07@users.noreply.github.com>
2022-04-03 12:01:25 +0100

    Refactored code and fixed warnings

--- src/App.js
+++ src/App.js
@@ -8,10 +8,21 @@ function App() {
       <div className="container">
         <Weather defaultCity="Sheffield" />
         <footer>
-          <a href="https://github.com/Adeola07/weather-react-project  target=_blank">
+          <a
+            href="https://github.com/Adeola07/weather-react-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Open-source code
           </a>{" "}
-          by Adeola Ejalonibu{" "}
+          by{" "}
+          <a
+            href="https://laughing-boyd-4e1917.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Adeola Ejalonibu
+          </a>
         </footer>
       </div>
     </div>
